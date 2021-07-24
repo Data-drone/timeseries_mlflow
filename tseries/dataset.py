f79c2cfa80a6c558559a29b47759d613941d01d6
Data-drone <bpl.law@gmail.com>
2021-07-25 01:11:43 +1000

    moved out the data munging into a dataset object

---
+++ tseries/dataset.py
@@ -0,0 +1,13 @@
+#
+# a Dataset primative for reuse
+#
+
+class Dataset:
+
+    def __init__():
+
+        raise NotImplementedError
+
+    def load_data():
+
+        raise NotImplementedError
---
+++ tseries/taxi_daily.py
@@ -0,0 +1,39 @@
+from tseries.dataset import Dataset
+from pyspark.sql import functions as F
+
+class TaxiDaily(Dataset):
+
+    def __init__(self, sparksession):
+
+        self.sparksession = sparksession
+
+    def load_data(self):
+
+        nyc_taxi_dataset = self.sparksession.sql("SELECT * FROM processed.nyc_taxi_dataset")
+        self.dataset = (
+            nyc_taxi_dataset
+                .withColumn("pickup_date", F.to_date("pickup_datetime"))
+                .select("pickup_date", "total_amount")
+                .groupBy("pickup_date")
+                .agg(
+                    F.count("total_amount").alias("total_rides"),
+                    F.sum("total_amount").alias("total_takings")
+                )
+                .sort("pickup_date")
+        )
+
+        self.train_data = (
+            self.dataset
+                .select(
+                    F.col("pickup_date").alias("ds"),
+                    F.col("total_rides").alias("y")
+                    )
+                .filter("pickup_date < '2014-07-01'"))
+
+        self.test_data = (
+            self.dataset
+                .select(
+                    F.col("pickup_date").alias("ds"),
+                    F.col("total_rides").alias("y")
+                    )
+                .filter("pickup_date >= '2014-07-01'"))
