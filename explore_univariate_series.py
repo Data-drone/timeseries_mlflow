f16228e38b65d6a10e956b8032f85c6e4272ba6c
Data-drone <bpl.law@gmail.com>
2021-08-13 00:54:37 +1000

    building out exploration notebook generator

--- explore_univariate_series.py
+++ explore_univariate_series.py
@@ -6,25 +6,57 @@
 
 
 import nbformat as nbf
+import inspect # To clean up indents
 
-def create_notebook_exploration():
+def create_notebook_exploration(setup_data = 'setup.py', 
+                                output_name = 'test.ipynb'):
     """
     
     A Function to create a programmatic data exploration notebook
+
+    We have specified a specific kernel spec that matches up with testing env
+
+    current assumptions:
+        Dataframes: train, val 
 
     """
 
     nb = nbf.v4.new_notebook()
 
-    text = """\
-    # My first automatic Jupyter Notebook
-    This is an auto-generated notebook."""
+    ############### First Section #######################
+    text = inspect.cleandoc("""# Automated Exploration Notebook \n
+        This is an automatically generate notebook""")
 
-    code = """\
-    %pylab inline
-    hist(normal(size=2000), bins=50);"""
+    import_spark_setup = "%run {0}".format(setup_data)
+
+    show_train_data = "train.head()"
+
+    ############# cells to explore data #################
+    explore_heading = "## View"
+
+    explore_dataset = inspect.cleandoc("""
+        # Using plotly.express
+        import plotly.express as px
+        
+        fig = px.line(train, x=train.index, y="total_rides")
+        fig.show()
+        """)
+
 
     nb['cells'] = [nbf.v4.new_markdown_cell(text),
-                   nbf.v4.new_code_cell(code) ]
+                   nbf.v4.new_code_cell(import_spark_setup),
+                   nbf.v4.new_code_cell(show_train_data),
+                   nbf.v4.new_markdown_cell(explore_heading),
+                   nbf.v4.new_code_cell(explore_dataset)]
 
-    nbf.write(nb, 'test.ipynb')
+    nb.metadata.kernelspec = {
+        "display_name": "Python [conda env:spark]",
+        "language": "python",
+        "name": "conda-env-spark-py"
+    }
+    
+    nbf.write(nb, output_name)
+
+
+if __name__ == '__main__':
+    create_notebook_exploration()
