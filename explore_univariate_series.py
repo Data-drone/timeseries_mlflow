5e6731203789e01167acb0760f008e76cf273cb7
Data-drone <bpl.law@gmail.com>
2021-08-13 17:39:14 +1000

    updated the setup file to run via functions and got the auto generated notebook working

--- explore_univariate_series.py
+++ explore_univariate_series.py
@@ -1,15 +1,19 @@
 #
 # A function to explore a univariate series and generate an exploratory notebook
 #
+# 
 #
-#
-
 
 import nbformat as nbf
 import inspect # To clean up indents
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from setup import start_spark, extract_data
+import os
 
-def create_notebook_exploration(setup_data = 'setup.py', 
-                                output_name = 'test.ipynb'):
+def create_notebook_exploration(df, setup_data = 'setup.py', 
+                                output_name = 'test.ipynb',
+                                ):
     """
     
     A Function to create a programmatic data exploration notebook
@@ -23,31 +27,52 @@ def create_notebook_exploration(setup_data = 'setup.py',
 
     nb = nbf.v4.new_notebook()
 
+    ############### Checks ##############
+    testing = inspect.cleandoc("""
+        import os \n
+        print(os.getcwd())
+    """)
+
     ############### First Section #######################
     text = inspect.cleandoc("""# Automated Exploration Notebook \n
         This is an automatically generate notebook""")
 
-    import_spark_setup = "%run {0}".format(setup_data)
+    import_spark_setup = inspect.cleandoc("""from setup import start_spark, extract_data \n
+        sparksesh = start_spark() \n
+        train, value = extract_data(sparksesh) \n
+        """) 
+    
+    #.format(setup_data)
 
     show_train_data = "train.head()"
 
     ############# cells to explore data #################
     explore_heading = "## View"
 
-    explore_dataset = inspect.cleandoc("""
+    #### Loop through and generate a chart per column
+    explore_data = []
+    for column in df.columns:
+        value = str(column)
+
+        explore_script = inspect.cleandoc("""
         # Using plotly.express
         import plotly.express as px
         
-        fig = px.line(train, x=train.index, y="total_rides")
+        fig = px.line(train, x=train.index, y='{0}')
         fig.show()
-        """)
+        """.format(value))
+
+        explore_data.append(nbf.v4.new_code_cell(explore_script))
 
 
-    nb['cells'] = [nbf.v4.new_markdown_cell(text),
-                   nbf.v4.new_code_cell(import_spark_setup),
-                   nbf.v4.new_code_cell(show_train_data),
-                   nbf.v4.new_markdown_cell(explore_heading),
-                   nbf.v4.new_code_cell(explore_dataset)]
+    nb['cells'] = [
+            nbf.v4.new_code_cell(testing),
+            nbf.v4.new_markdown_cell(text),
+            nbf.v4.new_code_cell(import_spark_setup),
+            nbf.v4.new_code_cell(show_train_data),
+            nbf.v4.new_markdown_cell(explore_heading)]
+
+    nb['cells'].extend(explore_data)
 
     nb.metadata.kernelspec = {
         "display_name": "Python [conda env:spark]",
@@ -57,6 +82,36 @@ def create_notebook_exploration(setup_data = 'setup.py',
     
     nbf.write(nb, output_name)
 
+    return output_name
+
+
+def run_notebook(output_name):
+
+    """
+    execute the notebook
+
+    """
+    
+
+    current_dir = os.getcwd()
+    
+    with open(output_name) as f:
+        nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+        ep.preprocess(nb, {'metadata': {'path': current_dir}})
+
+    with open('executed_notebook.ipynb', 'w', encoding='utf-8') as f:
+        nbformat.write(nb, f)
+
 
 if __name__ == '__main__':
-    create_notebook_exploration()
+
+    #load_func = 'setup'
+    #import setup
+    sparksesh = start_spark()
+    train, value = extract_data(sparksesh)
+
+    output_file = create_notebook_exploration(df=train) #, 
+    
+    run_notebook(output_file)
+    #setup_data = '{0}.py'.format(load_func))
--- setup.py
+++ setup.py
@@ -5,42 +5,41 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 import pandas as pd
 
-packages="""io.delta:delta-core_2.12:1.0.0,org.apache.hadoop:hadoop-aws:3.2.0"""
-os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages io.delta:delta-core_2.12:1.0.0,org.apache.hadoop:hadoop-aws:3.2.0 pyspark-shell"
 
-spark = SparkSession \
-            .builder \
-            .config("spark.executor.cores", 4) \
-            .config("spark.executor.memory", "4g") \
-            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-            .config("spark.master", "spark://spark-master:7077") \
-            .config("spark.hadoop.fs.s3a.access.key", os.environ['MINIO_ACCESS_KEY']) \
-            .config("spark.hadoop.fs.s3a.secret.key", os.environ['MINIO_SECRET_KEY']) \
-            .config("spark.hadoop.fs.s3a.endpoint", "minio:9000") \
-            .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
-            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-            .config("spark.hadoop.metastore.catalog.default", "hive") \
-            .config("spark.sql.warehouse.dir", "s3a://storage/warehouse") \
-            .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-            .config("spark.hadoop.fs.s3a.connection.maximum", "50") \
-            .config("spark.hive.metastore.uris", "thrift://192.168.64.4:9083") \
-            .appName("Jupyter Time Series") \
-            .enableHiveSupport() \
-            .getOrCreate()
+def start_spark():
+    packages="""io.delta:delta-core_2.12:1.0.0,org.apache.hadoop:hadoop-aws:3.2.0"""
+    os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages io.delta:delta-core_2.12:1.0.0,org.apache.hadoop:hadoop-aws:3.2.0 pyspark-shell"
 
-# set to cores to increase the efficiency
-spark.conf.set("spark.sql.shuffle.partitions", spark.sparkContext.defaultParallelism*4)
+    spark = SparkSession \
+                .builder \
+                .config("spark.executor.cores", 4) \
+                .config("spark.executor.memory", "4g") \
+                .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+                .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+                .config("spark.master", "spark://spark-master:7077") \
+                .config("spark.hadoop.fs.s3a.access.key", os.environ['MINIO_ACCESS_KEY']) \
+                .config("spark.hadoop.fs.s3a.secret.key", os.environ['MINIO_SECRET_KEY']) \
+                .config("spark.hadoop.fs.s3a.endpoint", "minio:9000") \
+                .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+                .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+                .config("spark.hadoop.metastore.catalog.default", "hive") \
+                .config("spark.sql.warehouse.dir", "s3a://storage/warehouse") \
+                .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+                .config("spark.hadoop.fs.s3a.connection.maximum", "50") \
+                .config("spark.hive.metastore.uris", "thrift://192.168.64.4:9083") \
+                .appName("Jupyter Time Series") \
+                .enableHiveSupport() \
+                .getOrCreate()
 
-## faster pandas data transfers
-spark.conf.set("spark.sql.execution.arrow.enabled", "true")
+    # set to cores to increase the efficiency
+    spark.conf.set("spark.sql.shuffle.partitions", spark.sparkContext.defaultParallelism*4)
+
+    ## faster pandas data transfers
+    spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+
+    return spark
 
 from tseries.taxi_daily import TaxiDaily
-
-taxi_daily = TaxiDaily(spark)
-taxi_daily.load_data()
-
-# MLflow setup
 
 # credentials for storing our model artifacts
 # mlflow needs these to be set whenever it is being called
@@ -48,22 +47,33 @@ os.environ['AWS_ACCESS_KEY_ID'] = os.environ.get('MINIO_ACCESS_KEY')
 os.environ['AWS_SECRET_ACCESS_KEY'] = os.environ.get('MINIO_SECRET_KEY')
 os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://minio:9000'
 
-starting_dataset = taxi_daily.dataset.filter("pickup_date < '2015-09-01'")
 
-split_date = '2015-08-01'
+def extract_data(spark):
+    """
+    
+    """
 
-train, val = starting_dataset.filter("pickup_date < '{0}'".format(split_date)).toPandas(), \
-                starting_dataset.filter("pickup_date >= '{0}'".format(split_date)).toPandas()
+    taxi_daily = TaxiDaily(spark)
+    taxi_daily.load_data()
 
-# set the dates to proper series
-train.index = train['pickup_date']
-train = train.drop(['pickup_date'], axis = 1)
-train.sort_index(inplace=True)
-train = train.asfreq('D')
+    # MLflow setup
+    starting_dataset = taxi_daily.dataset.filter("pickup_date < '2015-09-01'")
 
-val.index = val['pickup_date']
-val = val.drop(['pickup_date'], axis = 1)
-val.sort_index(inplace=True)
-val = val.asfreq('D')
+    split_date = '2015-08-01'
+    train, val = starting_dataset.filter("pickup_date < '{0}'".format(split_date)).toPandas(), \
+                    starting_dataset.filter("pickup_date >= '{0}'".format(split_date)).toPandas()
 
-spark.stop()
+    # set the dates to proper series
+    train.index = train['pickup_date']
+    train = train.drop(['pickup_date'], axis = 1)
+    train.sort_index(inplace=True)
+    train = train.asfreq('D')
+
+    val.index = val['pickup_date']
+    val = val.drop(['pickup_date'], axis = 1)
+    val.sort_index(inplace=True)
+    val = val.asfreq('D')
+
+    spark.stop()
+
+    return train, val
